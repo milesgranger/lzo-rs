dbe08edd4d5d0f82002652e5c9ab523637ab9327
milesgranger <miles59923@gmail.com>
2021-02-28 18:51:31 +0100

    Add decompress

--- src/lib.rs
+++ src/lib.rs
@@ -29,6 +29,20 @@ pub fn max_compress_len(input_len: usize) -> usize {
     input_len + (input_len / 16) + 64 + 3
 }
 
+pub fn decompress(input: &[u8], output: &mut [u8]) -> usize {
+    init();
+    unsafe {
+        let mut wrkmem : [u8; 0] = std::mem::uninitialized();
+        let mut out_len = 0;
+        let r = raw::lzo1x_decompress(input.as_ptr(), input.len() as u64, output.as_mut_ptr(), &out_len as *const _ as *mut _, wrkmem.as_mut_ptr() as *mut c_void);
+        if !r == 0 {
+            panic!("Failed to decompress, exit code: {}", r);
+        }
+        out_len as usize
+    }
+
+}
+
 pub fn compress(input: &[u8], output: &mut [u8]) -> usize {
     init();
     unsafe {
@@ -45,14 +59,20 @@ pub fn compress(input: &[u8], output: &mut [u8]) -> usize {
 
 #[cfg(test)]
 mod tests {
-    use crate::{compress, max_compress_len};
+    use crate::{compress, max_compress_len, decompress};
 
     #[test]
-    fn test_compress() {
+    fn test_roundtrip() {
         let input = b"Oh what a beautiful day, oh what a beaitufl morning!!!".to_vec();
-        let mut output = vec![0; max_compress_len(input.len())];
-        let n_bytes = compress(&input, output.as_mut_slice());
-        println!("{:?}", String::from_utf8_lossy(&output[..n_bytes].to_vec()));
+
+        let mut compressed = vec![0; max_compress_len(input.len())];
+        let n_bytes = compress(&input, compressed.as_mut_slice());
+        println!("{:?}", String::from_utf8_lossy(&compressed[..n_bytes].to_vec()));
+        println!("Output length: {}", n_bytes);
+
+        let mut decompressed: Vec<u8> = vec![0; input.len()];
+        let n_bytes = decompress(&compressed[..n_bytes], decompressed.as_mut_slice());
+        println!("{:?}", String::from_utf8_lossy(&decompressed[..n_bytes].to_vec()));
         println!("Output length: {}", n_bytes);
     }
 }
