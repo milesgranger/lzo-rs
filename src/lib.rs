a3f65f149603d65ac8bc9bea98f80c3e8621e795
milesgranger <miles59923@gmail.com>
2021-03-31 18:00:24 +0200

    Drop support for Encoder/Decoder

--- src/lib.rs
+++ src/lib.rs
@@ -81,87 +81,9 @@ pub fn compress(input: &[u8], output: &mut [u8], header: bool) -> usize {
     }
 }
 
-pub struct Decoder<R: io::Read> {
-    inner: R,
-    wrk_memory: [u8; 64000],
-    buf: [u8; 64000]
-}
-impl<R: io::Read> Decoder<R> {
-    pub fn new(inner: R) -> Self {
-        init();
-        Self {
-            inner,
-            wrk_memory: unsafe { std::mem::MaybeUninit::uninit().assume_init() },
-            buf: [0; 64000]
-        }
-    }
-}
-impl<R: io::Read> io::Read for Decoder<R> {
-    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
-        let n_bytes = self.inner.read(&mut self.buf)?;
-        if n_bytes == 0 {
-            return Ok(n_bytes);
-        }
-        let input = &self.buf[..n_bytes];
-        let mut out_len: u32 = 0;
-        unsafe {
-            let (r, _n_consumed_bytes) = raw::lzo1x_decompress_safe(
-                input.as_ptr(),
-                input.len() as u64,
-                buf.as_mut_ptr(),
-                &out_len as *const _ as *mut _,
-                self.wrk_memory.as_mut_ptr() as *mut c_void,
-            );
-            if r != 0 {
-                panic!("Failed to decompress, exit code: {}", r);
-            }
-            Ok(out_len as usize)
-        }
-    }
-}
-
-pub struct Encoder<R: io::Read> {
-    inner: R,
-    wrk_memory: [u8; 64000],
-    buf: [u8; 64000]
-}
-impl<R: io::Read> Encoder<R> {
-    pub fn new(inner: R) -> Self {
-        init();
-        Self {
-            inner,
-            wrk_memory: unsafe { std::mem::MaybeUninit::uninit().assume_init() },
-            buf: [0; 64000]
-        }
-    }
-}
-impl<R: io::Read> io::Read for Encoder<R> {
-    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
-        let n_bytes = self.inner.read(&mut self.buf)?;
-        if n_bytes == 0 {
-            return Ok(n_bytes);
-        }
-        let input = &self.buf[..n_bytes];
-        unsafe {
-            let mut out_len = 0;
-            let v = raw::lzo1x_1_compress(
-                input.as_ptr(),
-                input.len() as u64,
-                buf.as_mut_ptr(),
-                &out_len as *const _ as *mut _,
-                self.wrk_memory.as_mut_ptr() as *mut c_void,
-            );
-            if v != 0 {
-                panic!("Failed to compress, exit code: {}", v);
-            }
-            Ok(out_len as usize)
-        }
-    }
-}
-
 #[cfg(test)]
 mod tests {
-    use crate::{compress, decompress, max_compress_len, Encoder, Decoder};
+    use crate::{compress, decompress, max_compress_len};
 
     fn gen_data() -> Vec<u8> {
         (0..10000)
@@ -182,21 +104,5 @@ mod tests {
         let n_bytes = decompress(&compressed[..n_bytes], decompressed.as_mut_slice());
 
         assert_eq!(&decompressed[..n_bytes], input.as_slice());
-    }
-
-    #[test]
-    fn encoder_decoder_rountrip() {
-        let data = b"bytes".to_vec();
-        let mut encoder = Encoder::new(data.as_slice());
-        let mut compressed = vec![];
-        let n_bytes = std::io::copy(&mut encoder, &mut compressed).unwrap();
-        println!("{:?}", String::from_utf8_lossy(&compressed));
-
-        /*
-        let mut decompressed = vec![];
-        let mut decoder = Decoder::new(compressed.as_slice());
-        let n_bytes = std::io::copy(&mut decoder, &mut decompressed).unwrap();
-        assert_eq!(decompressed, data);
-        */
     }
 }
