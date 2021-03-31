acf3aa3dee225fdc3184bf27a421e32ddd3a2db9
milesgranger <miles59923@gmail.com>
2021-03-31 22:06:43 +0200

    Expand tests

--- src/lib.rs
+++ src/lib.rs
@@ -8,15 +8,16 @@
 extern crate libc;
 use std::convert::TryInto;
 use std::ffi::c_void;
-use thiserror::Error;
 
 pub(crate) mod raw;
 
 const BLOCK_SIZE: usize = 64000;
 const MAX_BLOCK_COMPRESS_SIZE: usize = BLOCK_SIZE + (BLOCK_SIZE / 16) + 64 + 3;
 
-#[derive(Error, Debug)]
-pub enum MiniLzoError {
+pub type Result<T> = std::result::Result<T, Error>;
+
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
     #[error("Header expected but not found.")]
     NoHeader,
     #[error("Invalid header format.")]
@@ -28,10 +29,10 @@ pub enum MiniLzoError {
 }
 
 // Required by LZO to be called before operations
-fn init() -> Result<(), MiniLzoError> {
+fn init() -> Result<()> {
     let r = unsafe { raw::lzo_initialize() };
     if r != 0 {
-        Err(MiniLzoError::LzoInit { code: r as i8 })
+        Err(Error::LzoInit { code: r as i8 })
     } else {
         Ok(())
     }
@@ -49,22 +50,20 @@ pub fn max_compress_len(input_len: usize) -> usize {
 /// Will always fail if the input does not contain a header; if this is your case, you'll need
 /// to pre-allocate a vec of appropriate length for decompression output and use [decompress](fn.decompress.html)
 /// directly.
-pub fn decompress_vec(input: &[u8]) -> Result<Vec<u8>, MiniLzoError> {
+pub fn decompress_vec(input: &[u8]) -> Result<Vec<u8>> {
     if [0xf0, 0xf1].contains(&input[0]) {
-        let length_bytes: [u8; 4] = input[1..5]
-            .try_into()
-            .map_err(|_| MiniLzoError::InvalidHeader)?;
+        let length_bytes: [u8; 4] = input[1..5].try_into().map_err(|_| Error::InvalidHeader)?;
         let mut output = vec![0; u32::from_be_bytes(length_bytes) as usize];
         let n = decompress(&input[5..], output.as_mut_slice())?;
         output.truncate(n);
         Ok(output)
     } else {
-        Err(MiniLzoError::NoHeader)
+        Err(Error::NoHeader)
     }
 }
 
 /// Decompress input into output. Will ignore any header if present in the input.
-pub fn decompress(input: &[u8], output: &mut [u8]) -> Result<usize, MiniLzoError> {
+pub fn decompress(input: &[u8], output: &mut [u8]) -> Result<usize> {
     init()?;
 
     // Determine if there is a header
@@ -85,7 +84,7 @@ pub fn decompress(input: &[u8], output: &mut [u8]) -> Result<usize, MiniLzoError
             wrkmem.as_mut_ptr() as *mut c_void,
         );
         if r != 0 {
-            return Err(MiniLzoError::LzoError { code: r as i8 });
+            return Err(Error::LzoError { code: r as i8 });
         }
         (out_len, n_consumed_bytes)
     };
@@ -94,7 +93,7 @@ pub fn decompress(input: &[u8], output: &mut [u8]) -> Result<usize, MiniLzoError
 
 /// Compress input into output buffer, optionally with a header written to the front of the output
 /// buffer.
-pub fn compress(input: &[u8], output: &mut [u8], header: bool) -> Result<usize, MiniLzoError> {
+pub fn compress(input: &[u8], output: &mut [u8], header: bool) -> Result<usize> {
     init()?;
 
     let mut out_len = 0;
@@ -114,7 +113,7 @@ pub fn compress(input: &[u8], output: &mut [u8], header: bool) -> Result<usize, 
         )
     };
     if r != 0 {
-        return Err(MiniLzoError::LzoError { code: r as i8 });
+        return Err(Error::LzoError { code: r as i8 });
     }
     if header {
         output[0] = 0xf0;
@@ -126,7 +125,7 @@ pub fn compress(input: &[u8], output: &mut [u8], header: bool) -> Result<usize, 
 
 /// Convenience function to compress input into an appropriately sized output buffer, optionally
 /// with a header.
-pub fn compress_vec(input: &[u8], header: bool) -> Result<Vec<u8>, MiniLzoError> {
+pub fn compress_vec(input: &[u8], header: bool) -> Result<Vec<u8>> {
     let mut output = vec![0; max_compress_len(input.len())];
     let n = compress(input, output.as_mut_slice(), header)?;
     output.truncate(n);
@@ -135,7 +134,7 @@ pub fn compress_vec(input: &[u8], header: bool) -> Result<Vec<u8>, MiniLzoError>
 
 #[cfg(test)]
 mod tests {
-    use crate::{compress, decompress, max_compress_len, compress_vec, decompress_vec};
+    use crate::{compress, compress_vec, decompress, decompress_vec, max_compress_len};
 
     fn gen_data() -> Vec<u8> {
         (0..100000)
@@ -145,11 +144,24 @@ mod tests {
     }
 
     #[test]
-    fn roundtrip_slices() {
+    fn roundtrip_slices_with_header() {
         let input = gen_data();
 
         let mut compressed = vec![0; max_compress_len(input.len())];
         let n_bytes = compress(&input, compressed.as_mut_slice(), true).unwrap();
+
+        let mut decompressed: Vec<u8> = vec![0; input.len()];
+        let n_bytes = decompress(&compressed[..n_bytes], decompressed.as_mut_slice()).unwrap();
+
+        assert_eq!(&decompressed[..n_bytes], input.as_slice());
+    }
+
+    #[test]
+    fn roundtrip_slices_without_header() {
+        let input = gen_data();
+
+        let mut compressed = vec![0; max_compress_len(input.len())];
+        let n_bytes = compress(&input, compressed.as_mut_slice(), false).unwrap();
 
         let mut decompressed: Vec<u8> = vec![0; input.len()];
         let n_bytes = decompress(&compressed[..n_bytes], decompressed.as_mut_slice()).unwrap();
@@ -164,11 +176,28 @@ mod tests {
         let decompressed = decompress_vec(compressed.as_slice()).unwrap();
         assert_eq!(decompressed, input);
     }
+
     #[test]
     fn rountrip_vecs_without_header() {
         let input = gen_data();
         let compressed = compress_vec(input.as_slice(), false).unwrap();
         let decompressed = decompress_vec(compressed.as_slice());
-        assert!(decompressed.is_err())  // decompress_vec needs to have a header.
+        assert!(decompressed.is_err()) // decompress_vec needs to have a header.
+    }
+
+    // expected output from python-lzo: lzo.compress(b'bytes')
+    #[test]
+    fn expected_format_with_header() {
+        let input = b"bytes".to_vec();
+        let output = compress_vec(input.as_slice(), true).unwrap();
+        assert_eq!(output, b"\xf0\x00\x00\x00\x05\x16bytes\x11\x00\x00")
+    }
+
+    // expected output from python-lzo: lzo.compress(b'bytes', 1, False) where 1 level and False header
+    #[test]
+    fn expected_format_without_header() {
+        let input = b"bytes".to_vec();
+        let output = compress_vec(input.as_slice(), false).unwrap();
+        assert_eq!(output, b"\x16bytes\x11\x00\x00")
     }
 }
